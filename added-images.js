a0fdb0d09158f5a1c558a0b2f70bf65e5c0a74ff
Kacper Sobania <kacper_s11@wp.pl>
2024-08-05 21:55:40 +0200

    Group files & add dividers

--- added-images.js
+++
@@ -1,7 +0,0 @@
-function onAddedImages(element) {
-    let labelContent = '';
-    if( element.files) {
-        labelContent = "Dodane zdjęcia: " + element.files.length;
-        document.getElementById("image-input-label").innerHTML = labelContent;
-    }
-}
